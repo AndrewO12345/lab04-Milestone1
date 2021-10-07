07cce74a4ad4c693a2f12d62f79ffaf44f93c196
Andrew <aoolson@wisc.edu>
2021-10-07 10:42:09 -0500

    Start button finished

--- app/src/main/java/com/example/lab04_milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab04_milestone1/MainActivity.java
@@ -5,25 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    class ExampleRunnable implements Runnable {
-        @Override
-        public void run() {
-            mockFileDownloader();
-        }
-    }
-
     private static final String TAG = "MainActivity";
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
     }
 
     public void mockFileDownloader() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.setText("Downloading...");
+            }
+        });
+
         for (int downloadProgress = 0; downloadProgress <= 100; downloadProgress=downloadProgress+10) {
             Log.d(TAG, "Download Progress: " + downloadProgress + "%");
             try {
@@ -31,6 +35,21 @@ public class MainActivity extends AppCompatActivity {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.setText("Start");
+            }
+        });
+
+    }
+
+    class ExampleRunnable implements Runnable {
+        @Override
+        public void run() {
+            mockFileDownloader();
         }
     }
 
