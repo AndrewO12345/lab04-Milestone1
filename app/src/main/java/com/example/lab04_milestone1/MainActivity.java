1274055297ea7fbb150c6116d361637fd4b4a1f9
Andrew <aoolson@wisc.edu>
2021-10-07 10:48:28 -0500

    Stop button finished

--- app/src/main/java/com/example/lab04_milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab04_milestone1/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private Button startButton;
+    private volatile boolean stopThread = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
         });
 
         for (int downloadProgress = 0; downloadProgress <= 100; downloadProgress=downloadProgress+10) {
+            if (stopThread) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startButton.setText("Start");
+                    }
+                });
+                return;
+            }
             Log.d(TAG, "Download Progress: " + downloadProgress + "%");
             try {
                 Thread.sleep(1000);
@@ -56,5 +66,9 @@ public class MainActivity extends AppCompatActivity {
     public void startDownload(View view) {
         ExampleRunnable runnable = new ExampleRunnable();
         new Thread(runnable).start();
+    }
+
+    public void stopDownload(View view) {
+        stopThread = true;
     }
 }
